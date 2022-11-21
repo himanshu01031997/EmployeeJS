d05e60c942341931959a9e55e556bfec89f3e03f
himanshu01031997 <hs010397@gmail.com>
2022-11-21 22:13:56 +0530

    UC1_Attendance

---
+++ employee.js
@@ -0,0 +1,13 @@
+const IS_ABSENT=0;
+
+let empcheck=Math.floor(Math.random()*10)%2;
+if(empcheck==IS_ABSENT)
+{
+    console.log("employee is absent");
+    return;
+
+}
+else
+{
+console.log("employee is present");
+}
